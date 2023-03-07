be477387328cc76948d6ef83f6060aae5813b9e5
haileykim2014 <haileykim2014@gmail.com>
2023-03-07 20:56:47 +0900

    서블릿 컨테이너 띄우기

--- src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -2,11 +2,18 @@ package tobyspring.helloboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.jetty.JettyServletWebServerFactory;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServer;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 
 public class HellobootApplication {
-
 	public static void main(String[] args) {
-		System.out.println("Hello Containerless Standalone application");
+//		TomcatServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+//		ServletWebServerFactory serverFactory = new JettyServletWebServerFactory();
+			ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+			WebServer webServer = serverFactory.getWebServer(); //다른 서블릿컨테이너도 지원하도록 추상화되어있다.
+			webServer.start();
 	}
 
 }
