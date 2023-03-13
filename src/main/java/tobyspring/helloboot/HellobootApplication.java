a9c89a7dc1c9af66a930f91735df8d1107af5160
haileykim2014 <haileykim2014@gmail.com>
2023-03-13 22:24:40 +0900

    자바코드 구성 정보사용

--- src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -3,13 +3,23 @@ package tobyspring.helloboot;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
-import org.springframework.web.context.support.GenericWebApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
-
+@Configuration
 public class HellobootApplication {
+  @Bean
+  public HelloController helloController(HelloService helloService){
+    return new HelloController(helloService);
+  }
+  @Bean
+  public HelloService helloService(){ //인터페이스 타입으로 리턴할것
+    return new SimpleHelloService();
+  }
   public static void main(String[] args) {
     //스프링 컨테이너 만들기
-    GenericWebApplicationContext applicationContext = new GenericWebApplicationContext(){
+    AnnotationConfigWebApplicationContext applicationContext = new AnnotationConfigWebApplicationContext(){
       @Override
       protected void onRefresh() {
         super.onRefresh();
@@ -17,15 +27,14 @@ public class HellobootApplication {
         ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
         WebServer webServer = serverFactory.getWebServer(servletContext -> {
           servletContext.addServlet("frontcontroller",
-              new DispatcherServlet(this)
-          ).addMapping("/*");
+              new DispatcherServlet(this))
+              .addMapping("/*");
 
         }); //다른 서블릿컨테이너도 지원하도록 추상화되어있다.
         webServer.start();
       }
     };
-    applicationContext.registerBean(HelloController.class);
-    applicationContext.registerBean(SimpleHelloService.class);
+    applicationContext.register(HellobootApplication.class);
     applicationContext.refresh();
   }
 
