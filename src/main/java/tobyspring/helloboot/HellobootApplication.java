69a3ffe8342f10e9ac6b5c959d32e7ac33bf2f10
haileykim2014 <haileykim2014@gmail.com>
2023-03-14 23:16:37 +0900

    SpringBootApplication - MySpringApplication class

--- src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -21,25 +21,7 @@ public class HellobootApplication {
   }
   public static void main(String[] args) {
     //스프링 컨테이너 만들기
-    AnnotationConfigWebApplicationContext applicationContext = new AnnotationConfigWebApplicationContext(){
-      @Override
-      protected void onRefresh() {
-        super.onRefresh();
-
-        ServletWebServerFactory serverFactory = this.getBean(ServletWebServerFactory.class);
-        DispatcherServlet dispatcherServlet = this.getBean(DispatcherServlet.class);
-//        dispatcherServlet.setApplicationContext(this); //스프링컨테이너가 applicationContext를 주입해준다.
-
-        WebServer webServer = serverFactory.getWebServer(servletContext -> {
-          servletContext.addServlet("frontcontroller",dispatcherServlet)
-              .addMapping("/*");
-
-        }); //다른 서블릿컨테이너도 지원하도록 추상화되어있다.
-        webServer.start();
-      }
-    };
-    applicationContext.register(HellobootApplication.class);
-    applicationContext.refresh();
+    MySpringApplication.run(HellobootApplication.class,args);
   }
 
 }
