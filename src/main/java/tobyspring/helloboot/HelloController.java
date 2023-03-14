541ad9d143d26b2dbbe8af064f444fcc521450f7
haileykim2014 <haileykim2014@gmail.com>
2023-03-14 22:51:08 +0900

    componentScan

--- src/main/java/tobyspring/helloboot/HelloController.java
+++ src/main/java/tobyspring/helloboot/HelloController.java
@@ -1,11 +1,14 @@
 package tobyspring.helloboot;
 
 import java.util.Objects;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@RequestMapping("/hello")
+@RestController
 public class HelloController {
   private final HelloService helloService;
 
@@ -13,8 +16,7 @@ public class HelloController {
     this.helloService = helloService;
   }
 
-  @GetMapping
-  @ResponseBody
+  @GetMapping("/hello")
   public String hello(String name){
     return helloService.sayHello(Objects.requireNonNull(name));
   }
--- src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -3,20 +3,13 @@ package tobyspring.helloboot;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
-import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 @Configuration
+@ComponentScan
 public class HellobootApplication {
-  @Bean
-  public HelloController helloController(HelloService helloService){
-    return new HelloController(helloService);
-  }
-  @Bean
-  public HelloService helloService(){ //인터페이스 타입으로 리턴할것
-    return new SimpleHelloService();
-  }
   public static void main(String[] args) {
     //스프링 컨테이너 만들기
     AnnotationConfigWebApplicationContext applicationContext = new AnnotationConfigWebApplicationContext(){
